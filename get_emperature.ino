e1b8ded6dc281fbdc5fde16f32bcfe5fabf93eb9
daniel-lukovskyy <daniel.lukovskyy@mail.huji.ac.il>
2025-08-31 12:13:34 +0300

    the first skatch that gets the tempreture

---
+++ get_emperature.ino
@@ -0,0 +1,62 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* WIFI_SSID = "iPhone";
+const char* WIFI_PASS = "12345678";
+
+String apiKey = "edf3b18260e9014b8f68c8509f63d36d";
+String city = "Jerusalem";
+String url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric";
+
+void setup() {
+
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("Connecting to WiFi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.println("...");
+  }
+  Serial.println("WiFi is connected");
+
+}
+
+void loop() {
+  if(WiFi.status() == WL_CONNECTED){
+    HTTPClient http;
+    http.begin(url);
+
+    Serial.println("Requesting for : " + url);
+    int httpCode = http.GET();
+
+    if(httpCode > 0){
+      Serial.print("HTTP response code: ");
+      Serial.println(httpCode);
+
+      String payload = http.getString();
+      Serial.println("Server response: " );
+      Serial.println(payload);
+  
+
+      int pos = payload.indexOf("\"temp\":");
+      if (pos != -1){
+        int end = payload.indexOf(",", pos);
+        String temp = payload.substring(pos + 7 ,end);
+        Serial.print("\nTemperature in ");
+        Serial.print(city);
+        Serial.print(": ");
+        Serial.print(temp);
+        Serial.println(" °C");
+      }
+
+    } else{
+      Serial.print("HTTP request error, code: ");
+      Serial.println(httpCode);
+    }
+    delay(10000);
+
+  }
+}
