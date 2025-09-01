b7f242a7b4d74b75493f1eac1157690c9ed13bc1
daniel-lukovskyy <daniel.lukovskyy@mail.huji.ac.il>
2025-09-02 00:30:40 +0300

    get_temperature + time.ino

--- get_emperature.ino
+++ get_emperature.ino
@@ -1,62 +1,75 @@
 #include <WiFi.h>
+#include <WiFiUdp.h>
 #include <HTTPClient.h>
+#include <NTPClient.h>
+#include <ArduinoJson.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-const char* WIFI_SSID = "YOUR WiFi";
-const char* WIFI_PASS = "YOUR PASSWORD";
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1); // &Wire is link for I^2 object ,-1 is res port
 
-String apiKey = "YOUR API";
+const char* SSID = "YOUR WiFi";
+const char* PASSWORD = "YOUR PASSWORD";
+
+String API_key = "YOUR API";
 String city = "YOUR CITY";
-String url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric";
+String url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API_key + "&units=metric";
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP,"pool.ntp.org", 3 * 3600, 60000);
 
 void setup() {
 
   Serial.begin(115200);
   delay(1000);
+  Serial.print("Connecting to WiFi...");
+  WiFi.begin(SSID,PASSWORD);
 
-  Serial.println("Connecting to WiFi...");
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-
-  while(WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED){
     delay(500);
-    Serial.println("...");
+    Serial.print("...");
   }
-  Serial.println("WiFi is connected");
-
+  Serial.println(WiFi.status());
+  timeClient.begin();
+  
 }
 
 void loop() {
-  if(WiFi.status() == WL_CONNECTED){
+  if (WiFi.status() == WL_CONNECTED){
     HTTPClient http;
     http.begin(url);
+    timeClient.update();
 
-    Serial.println("Requesting for : " + url);
-    int httpCode = http.GET();
+    
+    int http_code =http.GET(); //gets JSON,HTML or text
 
-    if(httpCode > 0){
-      Serial.print("HTTP response code: ");
-      Serial.println(httpCode);
-
-      String payload = http.getString();
-      Serial.println("Server response: " );
+    if (http_code > 0){
+      String payload =http.getString(); //saving body of the answer as string
       Serial.println(payload);
-  
 
-      int pos = payload.indexOf("\"temp\":");
-      if (pos != -1){
-        int end = payload.indexOf(",", pos);
-        String temp = payload.substring(pos + 7 ,end);
-        Serial.print("\nTemperature in ");
-        Serial.print(city);
-        Serial.print(": ");
-        Serial.print(temp);
-        Serial.println(" °C");
+      StaticJsonDocument<4096> doc; // making space for payload, 4096 is max
+      DeserializationError error = deserializeJson(doc, payload); // reads payload, saves in doc
+
+      //error_raise
+      if (error){
+        Serial.print("deserializeJson failed: " + String(error.f_str())); //.fstr returns text disc of error
+        return;
       }
 
-    } else{
-      Serial.print("HTTP request error, code: ");
-      Serial.println(httpCode);
-    }
-    delay(10000);
+      int hours = timeClient.getHours();
+      int minutes = timeClient.getMinutes();
+      
 
+      //getting data
+      float temperature = doc["main"]["temp"];
+
+      Serial.println("\n --Parsed Data--");
+      Serial.println("Temperature: " + String(temperature) + "C Time: " + String(hours) + ":" + String(minutes) + ":");
+    }
+
+    http.end();
   }
+  delay(60000);
 }
